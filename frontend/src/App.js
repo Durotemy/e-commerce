905c1a564dd08bd83ed89fdaef9f5d741fec8ebb
durotemy <durotemy@gmail.com>
2022-05-22 22:42:46 +0100

    third commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Route, Routes, } from "react-router-dom";
 import './App.css';
 import HomeScreens from './screens/HomeScreens';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from "./screens/CartScreen"
 
 const App = () => {
   return (
@@ -17,6 +18,8 @@ const App = () => {
             <Routes>
               <Route path='/' element={<HomeScreens />} exact />
               <Route exact path='/product/:id' element={<ProductScreen />} />
+              <Route path='/cart/:id' element={<CartScreen />} />
+              <Route path='/cart' element={<CartScreen />} />
             </Routes >
           </Container>
           <Footer />
